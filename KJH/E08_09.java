362eb9a97b4b0bc6b2494503b825749154384306
KimJinHye0n <63627253+KimJinHye0n@users.noreply.github.com>
2020-05-14 10:11:42 +0900

    Add files via upload

---
+++ KJH/E08_09.java
@@ -0,0 +1,30 @@
+package may0514;
+//object > Throwable > Exception > RuntimeException > ArithmeticException
+//runtimeexception은 언체크예외 / 선언따로할 이유 없음 의도적으로 오류 발생하기 위한 메소드
+class UnsupportedFunctionException extends RuntimeException {
+	final private int ERR_CODE = 100;
+	
+	UnsupportedFunctionException() {
+		super(); // 상위 메소드에서 저장할 수 있도록 설정
+	}
+	
+	UnsupportedFunctionException(String message, int x) {
+		super(message);
+	}
+	
+	public int getErrorCode() {
+		return ERR_CODE;
+	}
+	
+	@Override
+	public String getMessage() {
+		return "["+ getErrorCode() +"]"+super.getMessage();
+	}
+}
+
+
+public class E08_09 {
+	public static void main(String[] args) {
+		throw new UnsupportedFunctionException ("지원하지 않는 기능입니다.", 100); // 무조건 에러 발생
+	}
+}
